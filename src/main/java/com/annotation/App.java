469019e817e37c26967dac95673bbfba954a8279
Mesut Yakut <mst@mesutyakut.com>
2016-07-28 01:39:18 +0300

    First commit

---
+++ src/main/java/com/annotation/App.java
@@ -0,0 +1,10 @@
+package com.annotation;
+
+/**
+ * Created by mesut on 28.07.2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
